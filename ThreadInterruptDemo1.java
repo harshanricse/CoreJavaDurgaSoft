6deaf1b2b503b473e3452ee0499a8eae20e65b0e
harshanricse <tunuguntlaharsha@gmail.com>
2025-05-06 14:47:34 +0530

    interrupt() demo

---
+++ ThreadInterruptDemo1.java
@@ -0,0 +1,23 @@
+class MyThread extends Thread{
+	public void run(){
+		for(int i=0;i<10000;i++){
+			System.out.println("I am lazy thread"+i);
+		}
+		System.out.println("I want to sleep");
+		try{
+			Thread.sleep(10000);
+		}
+		catch(InterruptedException e){
+			System.out.println("I got interrupted");
+		}
+	}
+
+	}
+class ThreadInterruptDemo1
+{
+	public static void main(String []args){
+		Thread t = new MyThread();
+		t.start();
+		t.interrupt();
+	}
+}
