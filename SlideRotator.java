26340c081eb3ef9ebd8f66905572ea3b40dc5988
harshanricse <tunuguntlaharsha@gmail.com>
2025-05-06 14:18:49 +0530

    sleep() demo

---
+++ SlideRotator.java
@@ -0,0 +1,8 @@
+class SlideRotator{
+public static void main(String []args) throws InterruptedException{
+		for(int i=1;i<=10;i++){
+			System.out.println("Slide_ "+i);
+			Thread.sleep(5000);
+		}
+	}
+}
